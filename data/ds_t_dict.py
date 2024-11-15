d1ceecf061d977c2d1e09c951d03ecc7ffb07ec9
neocrz <neo59crz@gmail.com>
2024-11-15 15:53:46 -0300

    análises realizadas

---
+++ data/ds_t_dict.py
@@ -0,0 +1,82 @@
+variables = {
+    'GENDER': {
+        'description': 'Gênero do paciente',
+        'type': 'categorical',
+        'values': {'M': 0, 'F': 1}
+    },
+    'AGE': {
+        'description': 'Idade do paciente',
+        'type': 'numeric',
+        'values': 'Valor numérico correspondente à idade do paciente'
+    },
+    'SMOKING': {
+        'description': 'Paciente fuma?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'YELLOW_FINGERS': {
+        'description': 'O paciente tem dedos amarelos?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'ANXIETY': {
+        'description': 'Paciente tem ansiedade?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'PEER_PRESSURE': {
+        'description': 'Paciente sofre pressão social?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'CHRONIC_DISEASE': {
+        'description': 'Paciente tem alguma doença crônica?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'FATIGUE': {
+        'description': 'Paciente sente fadiga?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'ALLERGY': {
+        'description': 'Paciente tem alergias?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'WHEEZING': {
+        'description': 'Paciente tem chiado no peito?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'ALCOHOL_CONSUMING': {
+        'description': 'Paciente consome álcool?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'COUGHING': {
+        'description': 'Paciente tosse?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'SHORTNESS_OF_BREATH': {
+        'description': 'Paciente sente falta de ar?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'SWALLOWING_DIFFICULTY': {
+        'description': 'Paciente tem dificuldade para engolir?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'CHEST_PAIN': {
+        'description': 'Paciente sente dor no peito?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    },
+    'LUNG_CANCER': {
+        'description': 'Paciente tem câncer de pulmão?',
+        'type': 'binary',
+        'values': {'YES': 1, 'NO': 0}
+    }
+}
